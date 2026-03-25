2371bfe3604ac74992c4fc0ad5a77df13e2ecc07
Kayla-Ann Hurdle <kayla.ann.hurdle@gmail.com>
2026-03-25 14:42:26 -0500

    accounts for sample tests 2 through 7

--- Project03/PolyDrive.java
+++ Project03/PolyDrive.java
@@ -3,23 +3,115 @@ package edu.ilstu;
 public class PolyDriver {
 
 	public static void main(String[] args) {
+		MySingleLinkedList<Term> firstPoly = new MySingleLinkedList<>();
+		MySingleLinkedList<Term> secondPoly = new MySingleLinkedList<>();
+		
+		// Testing the add method on several tests
+		System.out.println("Sample output for the method add: \n");
 		//first sample output test
 		//first polynomials
-		MySingleLinkedList<Term> firstPoly = new MySingleLinkedList<>();
 		firstPoly.add(new Term(3,4));
 		firstPoly.add(new Term(2,2));
 		firstPoly.add(new Term(3,1));
 		firstPoly.add(new Term(7,0));
 		
 		//second polynomials
-		MySingleLinkedList<Term> secondPoly = new MySingleLinkedList<>();
 		secondPoly.add(new Term(2,3));
 		secondPoly.add(new Term(-5,1));
 		secondPoly.add(new Term(5,0));
 		
 		// displays first and second polynomials and what they look like added together
-		System.out.println("Sample output for the method add: ");
 		displayAllTerms(firstPoly, "First polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//second sample output test
+		//first polynomials
+		firstPoly.add(new Term(3,4));
+		firstPoly.add(new Term(2,2));
+		
+		//second polynomials
+		secondPoly.add(new Term(5,0));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//third sample output test
+		//first polynomials
+		firstPoly.add(new Term(2,2));
+		firstPoly.add(new Term(3,1));
+		firstPoly.add(new Term(7,0));
+		
+		//second polynomials
+		secondPoly.add(new Term(2,9));
+		secondPoly.add(new Term(3,0));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//forth sample output test
+		//first polynomials
+		firstPoly.add(new Term(2,3));
+		firstPoly.add(new Term(1,2));
+		
+		//second polynomials
+		secondPoly.add(new Term(2,3));
+		secondPoly.add(new Term(1,2));
+		secondPoly.add(new Term(1,1));
+		secondPoly.add(new Term(1,0));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//5th sample output test
+		//first polynomials
+		firstPoly.add(new Term(3,4));
+		firstPoly.add(new Term(7,0));
+		
+		//second polynomials
+		secondPoly.add(new Term(5,5));
+		secondPoly.add(new Term(2,2));
+		secondPoly.add(new Term(3,1));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//6th sample output test
+		//first polynomials
+		firstPoly.add(new Term(9,90));
+		
+		//second polynomials
+		secondPoly.add(new Term(1,1));
+		secondPoly.add(new Term(1,0));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
+		displayAllTerms(secondPoly, "Second polynomial");
+		displayAllTerms(add(firstPoly, secondPoly), "Sum");
+		
+		
+		//7th sample output test
+		//first polynomials
+		firstPoly.add(new Term(-2,-3));
+		
+		//second polynomials
+		secondPoly.add(new Term(-2,-2));
+		
+		// displays first and second polynomials and what they look like added together
+		displayAllTerms(firstPoly, "\nFirst polynomial");
 		displayAllTerms(secondPoly, "Second polynomial");
 		displayAllTerms(add(firstPoly, secondPoly), "Sum");
 		
@@ -29,7 +121,7 @@ public class PolyDriver {
 		MySingleLinkedList<Term> addedTerms = new MySingleLinkedList<>();
 		
 		// goes through each element in firstTerms list
-		while (firstTerms.size() > 0) {
+		while (firstTerms.size() > 0 && secondTerms.size() > 0) {
 			// Checks which term is greater and adds that term to the list.
 			// Then removes the greater term.
 			// If they are equal, create a new term with the coefficants added together
@@ -51,8 +143,14 @@ public class PolyDriver {
 			}
 		}
 		
+		// adds whatever is left from the firstTerms list
+		while(firstTerms.size() > 0) {
+			addedTerms.add(firstTerms.get(0));
+			firstTerms.remove(0);
+		}
+		
 		// adds whatever is left from the secondTerms list
-		for (int i = 0; i < secondTerms.size(); i++) {
+		while(secondTerms.size() > 0) {
 			addedTerms.add(secondTerms.get(0));
 			secondTerms.remove(0);
 		}
@@ -83,11 +181,20 @@ public class PolyDriver {
 	
 	public static void displayAllTerms(MySingleLinkedList<Term> terms, String text) {
 		System.out.print(text + " = ");
-        for (int i = 0; i < terms.size()-1; i++) {
-        		System.out.print(terms.get(i) + "+");
+		// prints first term
+		System.out.print(terms.get(0));
+		//prints every term after with addition sign in front if coefficant is positive
+        for (int i = 1; i < terms.size(); i++) {
+        	if(terms.get(i).getCoe() < 0) {
+        		System.out.print(terms.get(i));
+        	}
+        	else {
+        		System.out.print("+" + terms.get(i));
+        	}
+        		
         }
-        // prints last term without a plus sign
-        System.out.println(terms.get(terms.size()-1));
+        
+        System.out.println();
 	}
 
 }
