c16c69995d8b1c5daeed4b46f10e2f0866e9cf43
Kayla-Ann Hurdle <kayla.ann.hurdle@gmail.com>
2026-04-30 22:01:16 -0500

    Added in comments and fixed what the range was for random

--- Project04/Gadget.java
+++ Project04/Gadget.java
@@ -1,29 +1,49 @@
 /**
- * 
+ * ULID: <krhurdl>
+ * Class: IT 168 
  */
 package edu.ilstu;
 
 import java.util.Random;
 
 /**
- * 
+ * The Gadget class is responsible for keeping track of the price
+ * and ID. It also can return information about gadget formated.
  */
 public class Gadget {
 	private int price;
 	private int id;
 	private static int uniqueId;
 	
+	/**
+	 * Constructor for Gadget. Takes in material price and sets the
+	 * price to equal material price plus a random number 1 to 5 inclusive.
+	 * Assigns the gadget a unique ID. 
+	 * 
+	 * @param materialPrice for the month
+	 */
 	public Gadget(int materialPrice) {
 		Random rand = new Random();
 		this.uniqueId ++;
-		this.price = materialPrice + (rand.nextInt(5) + 1);
+		this.price = materialPrice + (rand.nextInt(5,11));
 		this.id = uniqueId;
 	}
 	
+	/**
+	 * Returns the price of the gadget.
+	 * 
+	 * @return the price of the gadget
+	 */
 	public int getPrice() {
 		return this.price;
 	}
 	
+	/**
+	 * Returns a string of information about the gadget including it's ID and price
+	 * while making sure spacing is consistent between all gadgets. 
+	 * 
+	 * @returns information about the gadget including it's ID and price
+	 */
 	public String toString() {
 		String output = "Gadget ID:" + id;
 		if(id < 10) {
--- Project04/Order.java
+++ Project04/Order.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * ULID: <krhurdl>
+ * Class: IT 168 
  */
 package edu.ilstu;
 
@@ -8,7 +9,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 /**
- * 
+ * The Order class keeps track of gadgets, order number,
+ * the list of gadgets for said order, and date the order
+ * was completed. 
+ * It also displays part or all of the order and allows the
+ * order to get updated as it is processed. 
  */
 public class Order {
 	private int gadgets;
@@ -17,6 +22,15 @@ public class Order {
 	private LocalDate orderDate;//when the order can be fulfilled
 	private static int uniqueId;
 	
+	/**
+	 * Constructor for Order class that takes in gadgets 
+	 * and current date. 
+	 * Sets the order number to a unique ID and the list
+	 * of gadgets called theOrder to null.
+	 * 
+	 * @param gadgets amount that the order requires
+	 * @param date that the order was created
+	 */
 	public Order(int gadgets, LocalDate date) {
 		uniqueId ++;
 		this.orderDate = date;
@@ -25,26 +39,59 @@ public class Order {
 		this.theOrder = null;
 	}
 	
+	/**
+	 * Returns the amount of gadgets required for order.
+	 * 
+	 * @return the amount of gadgets required for order
+	 */
 	public int getGadgets() {
 		return this.gadgets;
 	}
 	
+	/**
+	 * Returns the date the order was fulfilled.
+	 * 
+	 * @return the date the order was fulfilled
+	 */
 	public LocalDate getDate() {
 		return this.orderDate;
 	}
 	
+	/**
+	 * Allows for the date to be set to a new date
+	 * if order was completed on a day other than 
+	 * when it was made. 
+	 * 
+	 * @param date the order was fulfilled
+	 */
 	public void setDate(LocalDate date) {
 		this.orderDate = date;
 	}
 	
+	/**
+	 * Fulfills the order by taking in the list of gadgets given to 
+	 * fulfill the order and setting theOrder to that. 
+	 * 
+	 * @param theOrder list of gadgets that were given to fulfill the order
+	 */
 	public void fulfillOrder(List<Gadget> theOrder) {
 		this.theOrder = theOrder;
 	}
 	
+	/**
+	 * Returns a string of the order number and gadgets ordered labeled.
+	 * @return a string of the order number and gadgets ordered labeled.
+	 */
 	public String getOrder() {
-		return "Order number: " + orderNumber + "    gadgets ordered: " + gadgets;
+		return "Order number:  " + orderNumber + "    gadgets ordered:  " + gadgets;
 	}
 	
+	/**
+	 * Returns a string with information about the order including all gadgets and order 
+	 * total cost. 
+	 * 
+	 * @return Information about the order including all gadgets and order total cost.
+	 */
 	public String toString() {
 		DecimalFormat format = new DecimalFormat("$####0.00");
 		
@@ -70,6 +117,12 @@ public class Order {
 		return output;
 	}
 	
+	/**
+	 * Calculates the total price of this order by adding up the price of each
+	 * gadget in the order. 
+	 * 
+	 * @return total price of this order
+	 */
 	public double calcPrice() { 
 		final double TAXES = .08;
 		double costTotal = 0;
